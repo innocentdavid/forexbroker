1f16674366b91ef722c81a3cc05438910fa573fa
innocentdavid <paulinnocent04@gmail.com>
2023-05-20 11:05:58 +0100

    d

--- src/components/tabledata.jsx
+++ src/components/tabledata.jsx
@@ -9,25 +9,25 @@ import IKR from '../images/forex.com logo.jpg'
 
 
 
-export default function Table(){
-    return(
-                <div className='mt-[60px] md:max-w-[1000px]  w-full  md:mx-auto '>
-                    <h1 className='md:text-[35px] mt-[50px] text-[25px] md:px-4 px-3 font-semi-bold'>ForexBrokers.com 2023 Overall Ranking</h1>
+export default function Table() {
+    return (
+        <div className='mt-[60px] md:max-w-[1000px]  w-full  md:mx-auto '>
+            <h1 className='md:text-[35px] mt-[50px] text-[25px] md:px-4 px-3 font-semi-bold'>iLiveforex.com 2023 Overall Ranking</h1>
 
-                    <p className='md:text-[20px] text-[18px] md:px-5 px-3 md:my-4 my-2'>Now that you've seen our picks for the best forex brokers, check out the ForexBrokers.com Overall Rankings. We've evaluated over 60 forex brokers, using a testing methodology that's based on 100+ data-driven variables and thousands of data points. Check out our full-length, in-depth <a href='/' className='text-blue-700'>forex broker reviews.</a></p>
+            <p className='md:text-[20px] text-[18px] md:px-5 px-3 md:my-4 my-2'>Now that you've seen our picks for the best forex brokers, check out the iLiveforex.com Overall Rankings. We've evaluated over 60 forex brokers, using a testing methodology that's based on 100+ data-driven variables and thousands of data points. Check out our full-length, in-depth <a href='/' className='text-blue-700'>forex broker reviews.</a></p>
 
-                    <div className='mt-[45px] max-w-[900px] '>
-                        
-                        <div className='gap-7 mt-[80px]'>
-                     
-                            <TableWithScroll/>
+            <div className='mt-[45px] max-w-[900px] '>
 
-                           
-                        </div>
+                <div className='gap-7 mt-[80px]'>
 
-                    </div>
+                    <TableWithScroll />
+
 
                 </div>
+
+            </div>
+
+        </div>
 
     )
 
